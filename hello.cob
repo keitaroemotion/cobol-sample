0d7d6994031dd214b43dc4b5025ec9d8a88d6520
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-11 17:31:09 +0900

    character length update

--- hello.cob
+++ hello.cob
@@ -4,12 +4,13 @@
       environment    division.
       data           division.
           working-storage section.
-              01 str  pic x(15) value "hello world".
-              01 name pic x(30).
+              01 str  pic x(20) value "Listen to my music".
+              01 name pic x(60).
       procedure      division.
            display str.
            move    "https://www.amazon.com/Sushimagnus-Keita-Roimo/dp/B00VTLVQPM" to name.
       
       hello.
+          display name.
           stop    run.
 
