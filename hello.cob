2ca6c78666ad587b57a37e53c66a1c08d65b7cb2
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-11 17:35:15 +0900

    raise question

--- hello.cob
+++ hello.cob
@@ -4,13 +4,16 @@
       environment    division.
       data           division.
           working-storage section.
-              01 str  pic x(20) value "Listen to my music".
-              01 name pic x(60).
+              01 str    pic x(20) value "Listen to my music".
+              01 name   pic x(60).
+              01 answer pic x(1).
       procedure      division.
            display str.
            move    "https://www.amazon.com/Sushimagnus-Keita-Roimo/dp/B00VTLVQPM" to name.
+           display name.
+           display "You wanna buy it? [Y/n]".
+           accept  answer.
       
       hello.
-          display name.
           stop    run.
 
